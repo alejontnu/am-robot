cb77696e05d61840de332270b23e03f1eb060e0f
Alexander <55593551+alejontnu@users.noreply.github.com>
2021-09-15 14:56:46 +0200

    added numpy to install requires

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setuptools.setup(
     package_dir={"": "src"},
     #package_data={'': ['benchmark.txt']},
     include_package_data=True,
-    install_requires=["frankx","argparse"],
+    install_requires=["frankx","argparse","numpy"],
     python_requires=">=3.6",
 )
