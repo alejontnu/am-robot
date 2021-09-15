eba7c57e845347fd0107e6c3ddc8f22f3fc97234
Alexander <alro2011@hotmail.com>
2021-09-15 14:31:58 +0200

    Updated gitignore and setup

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
-todo
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="am-robot",
+    version="0.0.1",
+    author="Alexander",
+    author_email="alro2011@hotmail.com",
+    description="Controller package for using the Franka Emika Panda robot manipulator for additive manufacturing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alejontnu/am-robot",
+    project_urls={
+        "Bug Tracker": "https://github.com/alejontnu/am-robot/issues",
+    },
+    entry_points = {
+        'console_scripts': ['run=am-robot.command_line:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    #package_data={'': ['benchmark.txt']},
+    include_package_data=True,
+    install_requires=["frankx","argparse"],
+    python_requires=">=3.6",
+)
