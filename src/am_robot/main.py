de873fbf4597278280cae58a4bebe5bd4137f53b
Alexander Johnsgaard <alro2011@hotmail.com>
2021-09-21 16:40:42 +0200

    started gcode parsing

---
+++ src/am_robot/main.py
@@ -0,0 +1,38 @@
+import os
+# import am_robot
+# from gcodeparser import GcodeParser
+
+# TODO - filename from argparse
+
+def main():
+    filename = 'circles.gcode'
+    folder = 'data'
+
+    print(os.getcwd())
+
+    fullpath = os.path.join('.',folder,filename)
+    print(fullpath)
+    print(os.listdir('data'))
+
+    # for root, dirs, files in os.walk(".", topdown=True):
+    #     print('start')
+    #     for dir in dirs:
+    #         print(os.path.join(root, dir))
+    #     print('stop-start')
+    #     for file in files:
+    #         print(os.path.join(root, file))
+    #     print('stop')
+
+
+
+    # print(am_robot.__file__.split)
+
+    with open(fullpath,'r') as file:
+        gcode = file.read()
+
+
+    # parsed_gcode = GcodeParser(gcode)
+    # print(parsed_gcode.lines)
+
+if __name__ == '__main__':
+    main()
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,6 +1,4 @@
 import pytest
 
-import numpy
-
 def test_add_numbers():
   assert (3 + 3) == 6
