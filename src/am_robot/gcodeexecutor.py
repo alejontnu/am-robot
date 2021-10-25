93a5473e1c70fefbc073e663163d086877055c3d
Alexander <alro2011@hotmail.com>
2021-10-25 19:14:03 +0200

    executor changes, visulization start

--- src/am_robot/gcodeexecutor.py
+++ src/am_robot/gcodeexecutor.py
@@ -18,8 +18,7 @@ class GCodeExecutor:
 
 	'''
 	def __init__(self,filename):
-		initial_interval = [0,0]
-		self.interval = initial_interval
+
 
 		file_extension = '.gcode'
 		if file_extension not in filename:
@@ -30,7 +29,9 @@ class GCodeExecutor:
 		with open(fullpath,'r') as file:
 			self.gcodelines = GcodeParser(file.read()).lines
 
-		# scan through sections at a time
+		initial_interval = [0,0]
+		self.interval = initial_interval
+		self.list_of_intervals = []
 		self.filename = filename
 		self.number_of_lines = len(self.gcodelines)
 
@@ -49,8 +50,14 @@ class GCodeExecutor:
 		command = self.get_command(interval[0])
 		self.command = command
 
-	def get_params(self,line_number,**param):
-		return 0
+	def get_params(self,line_number,param):
+		if param in self.gcodelines[line_number].params:
+			return self.gcodelines[line_number].params[param]
+		else:
+			return 0
+
+	def set_params(self,line_number,param):
+		self.__dict__[param] = self.get_params(line_number,param)
 
 	def make_waypoints(self):
 		return 0
@@ -60,30 +67,39 @@ class GCodeExecutor:
 		return 0
 
 	# Find the next instance of retraction (NB may be 0mm retraction)
-	def find_interval(self):
+	def find_next_interval(self):
 		previous_interval = self.interval
 
 		if previous_interval[1]+1 >= self.number_of_lines:
+			print("End of code")
 			return 'End of code' 
 		
 		counter = previous_interval[1]+1
+		interval = [counter,counter]
 		while (counter < self.number_of_lines):
 			if self.get_command(counter) != self.get_command(previous_interval[1]+1):
 				interval = [previous_interval[1]+1,counter-1]
-				print(interval)
-				self.set_interval(interval)
-				return interval
-			elif self.get_params(counter) != self.get_params(previous_interval[1]+1):
+				print("interval end due to command change")
+				break
+			elif self.get_params(counter,'F') != 0 and self.get_params(counter,'F') != self.F:
 				interval = [previous_interval[1]+1,counter-1]
-				print(interval)
-				self.set_interval(interval)
-				return interval
+				self.set_params(counter,'F')
+				print("interval end due to F param change")
+				break
+			elif self.get_params(counter,'E') < self.get_params(counter-1,'E'):
+				interval = [previous_interval[1]+1,counter-1]
+				self.set_params(counter,'E')
+				print("interval end due to E param change")
+				break
 			else:
 				counter = counter + 1
 
-		
-		print(interval)
 		self.set_interval(interval)
+		self.append_interval()
+
+
+	def append_interval(self):
+		self.list_of_intervals.append(self.interval)
 
 	def __str__(self):
 		return "gcodeexecutor"
@@ -92,20 +108,29 @@ class GCodeExecutor:
 		print(f"Name of file being processed: {self.filename}")
 		print(f"Current interval: {self.interval}")
 
+	def visualize_gcode(self):
+		return 0
+
 
 if __name__ == '__main__':
 	filename = 'Circle'
 	executioner = GCodeExecutor(filename)
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
-	executioner.find_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+	executioner.find_next_interval()
+
+	print("end of intervals")
+	print(executioner.list_of_intervals)
